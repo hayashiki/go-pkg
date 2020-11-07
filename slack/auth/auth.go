d268cb99f9883190f5c6c3a6cf821c55c7d447dc
hayashiki <masayuki@hayashida.dev>
2020-11-07 10:46:09 +0900

    Implement slack auth

---
+++ slack/auth/auth.go
@@ -0,0 +1,79 @@
+package auth
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const (
+	SlackDefaultAPIURL = "https://slack.com"
+)
+
+type (
+	Auth interface {
+		Redirect(w http.ResponseWriter, r *http.Request) error
+		CallbackHTML(TeamID string) string
+	}
+
+	auth struct {
+		state       string
+		clientID    string
+		redirectURL string
+		baseURL     string
+		scopes      []string
+	}
+
+	AuthorizeResponse struct {
+		Code  string
+		State string
+	}
+)
+
+func (c *auth) accessURL() (string, error) {
+	u, err := url.Parse(c.baseURL)
+	u.Path = "oauth/v2/authorize"
+	if err != nil {
+		return "", err
+	}
+
+	v := url.Values{}
+	v.Add("client_id", c.clientID)
+	v.Add("redirect_uri", c.redirectURL)
+	v.Add("state", c.state)
+	v.Add("scope", strings.Join(c.scopes, ","))
+	u.RawQuery = v.Encode()
+
+	return u.String(), nil
+}
+
+// Redirect redirect to request url
+func (c *auth) Redirect(w http.ResponseWriter, r *http.Request) error {
+	urlStr, err := c.accessURL()
+	if err != nil {
+		return err
+	}
+	http.Redirect(w, r, urlStr, http.StatusTemporaryRedirect)
+	return nil
+}
+
+func ParseRequest(r *http.Request) *AuthorizeResponse {
+	return &AuthorizeResponse{
+		Code:  r.FormValue("code"),
+		State: r.FormValue("state"),
+	}
+}
+
+func (c *auth) CallbackHTML(TeamID string) string {
+	panic("implement me")
+}
+
+func NewAuth(clientID, redirectURL, state string, scopes []string) Auth {
+	return &auth{
+		clientID:    clientID,
+		redirectURL: redirectURL,
+		baseURL:     SlackDefaultAPIURL,
+		scopes:      scopes,
+		state:       state,
+	}
+}
